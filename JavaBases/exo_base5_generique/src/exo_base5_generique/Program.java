3699dfe49b8127379c6a34a12ccf8fe0475dfa1e
otman el <otman-82@hotmail.fr>
2017-07-07 17:01:52 +0200

    first commit

---
+++ JavaBases/exo_base5_generique/src/exo_base5_generique/Program.java
@@ -0,0 +1,10 @@
+package exo_base5_generique;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaBases/exo_base5_generique/src/exo_base5_generique/metiers/Rectangle.java
@@ -0,0 +1,5 @@
+package exo_base5_generique.metiers;
+
+public class Rectangle extends Figure{
+
+}
