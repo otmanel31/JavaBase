d003651aa1ad61177c7c8a5246c2a3779c5f4481
otman badman <otman-82@hotmail.fr>
2017-07-26 20:40:50 +0200

    home work in progress

--- JavaBases/exo9_2_daoBuilder/src/exo9_2_daoBuilder/DaoBuilder.java
+++ JavaBases/exo9_2_daoBuilder/src/exo9_2_daoBuilder/DaoBuilder.java
@@ -1,6 +1,10 @@
 package exo9_2_daoBuilder;
 
+import java.lang.reflect.Method;
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class DaoBuilder implements IDao{
 	private Class className;
@@ -18,6 +22,21 @@ public class DaoBuilder implements IDao{
 		setClassName(className);
 		setConnexion(connexion);
 		setPrimaryKeyName(primaryKeyName);
+		generate();
+	}
+	
+	public void generate(){
+		String tableName = this.getClassName().getSimpleName().toLowerCase();
+		Method[] methods;
+		methods = this.getClassName().getDeclaredMethods();
+		Arrays.asList(methods).stream().forEach(f->{
+			if (f.getName().matches("^get[A-Z].*") && !f.getReturnType().equals(void.class))
+				System.out.println(f.getName().substring(3).toLowerCase());
+			if( f.getName().matches("is[A-Z].*") && !f.getReturnType().equals(void.class))
+				System.out.println("bool " + f.getName().substring(2).toLowerCase());
+		});
+		//Arrays.asList(methods).stream().forEach(f-> f.toString().substring(3));
+		//System.out.println(tableName);
 	}
 	
 	public void ignoreChamp() {
@@ -44,5 +63,25 @@ public class DaoBuilder implements IDao{
 		
 	}
 	
+	/*public static class Builder{
+	private Class buildClassName;
+	private Connection connexion;
+	private String buildPrimaryKey;
+	private List<String> attributs;
 	
+	public Builder(Class className, Connection connexion, String primaryKey){
+		this.buildClassName = className;
+		this.connexion = connexion;
+		this.buildPrimaryKey = primaryKey;
+		this.attributs = new ArrayList<>();
+	}
+	
+	
+	
+	public DaoBuilder build(){
+		return null;
+	}
+	
+	}
+	 */
 }
